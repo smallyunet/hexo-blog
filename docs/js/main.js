4bedf97253fa3b18180fbc2c54f001674edae6dc
smallyunet <499503799@qq.com>
2020-02-29 17:55:10 +0800

    m

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,23 @@
 if (/mobile/i.test(navigator.userAgent) || /android/i.test(navigator.userAgent)) {
   document.body.classList.add('mobile')
 }
+
+// 阅读时长统计
+(() => {
+  var e = document.getElementById('time-statistics')
+  var second = 0
+  var render = () => { 
+    var s = ''
+    if (second < 60) {
+      s = `${second}秒`
+    } else if (second < 3600) {
+      s = `${parseInt(second/60)}分钟`
+    } else if (second < 3600) {
+      s = `${parseInt(second/3600)}小时`
+    }
+    e.innerHTML = s
+    second += 1
+  }
+  setInterval(render, 1000);
+})()
+
