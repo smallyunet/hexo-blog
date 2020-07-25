a8ea64ecfb712364f5d0a95c84d392d51214621c
smallyunet <499503799@qq.com>
2020-07-25 22:39:21 +0800

    sh提交 自动commit

--- themes/yinwang/source/js/micro-blog/main.js
+++ themes/yinwang/source/js/micro-blog/main.js
@@ -5,7 +5,7 @@ $(() => {
     let ul = $('#micro-blog ul')
 
     let loadingEl = `
-        <div class="progress" style="margin-top:20%;">
+        <div class="progress">
             <div id="loading" class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
             </div>
         </div>
@@ -20,20 +20,25 @@ $(() => {
     }, 200)
 
     let url = 'https://api.github.com/gists/73edf8cf12b58cb88afd86a72f79bd85/comments'
-    $.get(url, res => {
-        loading.css('width', "100%")
-        clearInterval(timer)
-        ul.html(``)
+    $.ajax({
+        url: url,
+        success: res => {
+            loading.css('width', "100%")
+            clearInterval(timer)
+            ul.html(``)
 
-        res.map(i => {
-
-            let item = `<li class="list-group-item title">`
-            item += `<div class="date">${new Date(i.created_at)}</div>`
-            item += `<div style="margin-top:5px;">${i.body}</div>`
-            item += `</li>`
-
-            ul.append(item)
-        })
+            res.map(i => {
+                let item = `<li class="list-group-item title">`
+                item += `<div class="date">${new Date(i.created_at)}</div>`
+                item += `<div style="margin-top:5px;">${i.body}</div>`
+                item += `</li>`
+                ul.append(item)
+            })
+        },
+        error: () => {
+            clearInterval(timer)
+            ul.html(`网络异常，请刷新页面重试。 <a href="/micro-blog">点击刷新</a>`)
+        }
     })
 
 })
