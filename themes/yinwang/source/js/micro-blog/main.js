95c2b8a65bb732c1aeab71ff784ad6bb96d6a2c4
smallyunet <499503799@qq.com>
2020-07-26 15:53:11 +0800

    sh提交 自动commit

--- themes/yinwang/source/js/micro-blog/main.js
+++ themes/yinwang/source/js/micro-blog/main.js
@@ -1,37 +1,47 @@
 
+Date.prototype.format = function(fmt) { 
+    var o = { 
+       "M+" : this.getMonth()+1,                 //月份 
+       "d+" : this.getDate(),                    //日 
+       "h+" : this.getHours(),                   //小时 
+       "m+" : this.getMinutes(),                 //分 
+       "s+" : this.getSeconds(),                 //秒 
+       "q+" : Math.floor((this.getMonth()+3)/3), //季度 
+       "S"  : this.getMilliseconds()             //毫秒 
+   }; 
+   if(/(y+)/.test(fmt)) {
+           fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+   }
+    for(var k in o) {
+       if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+   return fmt; 
+} 
 
 $(() => {
 
     let ul = $('#micro-blog ul')
-
-    let loadingEl = `
-        <div class="progress">
-            <div id="loading" class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
+    ul.append(`
+        <div style="text-align:center;">
+            <div class="loadingio-spinner-ripple-8txk08frrfa">
+                <div class="ldio-fwkeq5l2tj8"><div></div><div></div></div>
             </div>
         </div>
-    `
-    ul.append(loadingEl)
-    let loading = $('#micro-blog ul #loading')
-
-    let w = 0
-    let timer = setInterval(() => {
-        w += 5
-        loading.css('width', `${w}%`)
-    }, 200)
+    `)
 
     let url = 'https://api.github.com/repos/smallyunet/hexo-blog/issues/7/comments'
     $.ajax({
         url: url,
         success: res => {
-            console.log(res)
-
-            loading.css('width', "100%")
-            clearInterval(timer)
             ul.html(``)
-
             res.map(i => {
+
+                let date = new Date(i.created_at).format("yyyy年MM月dd日 hh:mm:ss")
+
                 let item = `<li class="list-group-item title">`
-                item += `<div class="date">${new Date(i.created_at)}</div>`
+                item += `<div class="date">${date}</div>`
                 item += `<div style="margin-top:5px;">${i.body}</div>`
                 item += `</li>`
                 ul.append(item)
