d467b083324f5c649b6a5fdda6cc4c7fab0c2c58
smallyu <wangyudev@qq.com>
2020-12-11 12:49:56 +0800

    auto commit

---
+++ themes/yinwang/scripts/index.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var https = require('https');
+fs = require('fs')
+
+var options = {
+	host: 'api.github.com',
+	path: '/repos/smallyunet/hexo-blog/issues/7/comments',
+	method: 'GET',
+	headers: {'user-agent': 'node.js'}
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been received, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been received, so we just print it out here
+  response.on('end', function () {
+		fs.writeFile('../../../source/micro-blog/2020.json', str, err => {
+			if (err) {
+					console.log(err)
+			} else {
+					console.log(`Get 2020 microblog data success.`)
+			}
+		})
+  });
+}
+
+https.request(options, callback).end();
