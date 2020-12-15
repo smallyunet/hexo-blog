35dc4b470bca6e9acce7e575bf862f4bb9604568
smallyu <499503799@qq.com>
2020-12-15 05:57:40 +0000

    auto commit

--- themes/yinwang/scripts/index.js
+++ themes/yinwang/scripts/index.js
@@ -1,32 +1,35 @@
-var http = require('http');
-var https = require('https');
-fs = require('fs')
+var http = require("http");
+var https = require("https");
+fs = require("fs");
 
 var options = {
-	host: 'api.github.com',
-	path: '/repos/smallyunet/hexo-blog/issues/7/comments?per_page=100',
-	method: 'GET',
-	headers: {'user-agent': 'node.js'}
+  host: "api.github.com",
+  path: "/repos/smallyunet/hexo-blog/issues/7/comments?per_page=100",
+  method: "GET",
+  headers: { "user-agent": "node.js" },
 };
 
-callback = function(response) {
-  var str = '';
+callback = function (response) {
+  var str = "";
 
-  //another chunk of data has been received, so append it to `str`
-  response.on('data', function (chunk) {
+  response.on("data", function (chunk) {
+	chunk = chunk.toString('utf-8');
     str += chunk;
   });
 
-  //the whole response has been received, so we just print it out here
-  response.on('end', function () {
-		fs.writeFile('./source/micro-blog/2020.json', str, err => {
-			if (err) {
-					console.log(err)
-			} else {
-					console.log(`Get 2020 microblog data success.`)
-			}
-		})
+  response.on("end", function () {
+    fs.writeFile("./source/micro-blog/2020.json", str, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Get 2020 microblog data success.`);
+      }
+    });
   });
-}
+
+  response.on("error", function (err) {
+    next(err);
+  });
+};
 
 https.request(options, callback).end();
