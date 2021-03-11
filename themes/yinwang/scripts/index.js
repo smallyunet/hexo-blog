63bb352af17f1c844da3f0eecb3f1af9709a559b
smallyu <wangyudev@qq.com>
2021-03-11 22:35:11 +0800

    auto commit

--- themes/yinwang/scripts/index.js
+++ themes/yinwang/scripts/index.js
@@ -23,7 +23,7 @@ callback = function (response) {
 
   response.on("data", function (buf) {
     buffer = Buffer.concat([buffer, buf])
-    console.log('[micro-blog] Get something...');
+    console.log(`[micro-blog] ${Date.now()} Got something...`);
   });
 
   response.on("end", function () {
