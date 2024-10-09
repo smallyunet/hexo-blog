bdb06f4440049af5adc8e2a9c0fa3fc3f8b428fa
smallyunet <dev.wangyu@gmail.com>
2024-10-09 20:01:33 +0800

    remove js reward

--- themes/yinwang/source/js/main.js
+++ themes/yinwang/source/js/main.js
@@ -31,18 +31,6 @@ function preloadMicroBlog(year, forceReload = false) {
   }
 }
 
-const startPrivateKey = BigInt("0x4968be1cc4ac9f00a12c5294ebe7a92d6a67c78beea662a1a3e2769a9ac42a00");
-const endPrivateKey = BigInt("0x4968be1cc4ac9f00a12c5294ebe7a92d6a67c78beea662a1a3e2769a9ac42aff");
-function generateRandomPrivateKey() {
-  const range = endPrivateKey - startPrivateKey + BigInt(1);
-  const randomBigInt = BigInt(Math.floor(Math.random() * Number(range)));
-  const randomPrivateKey = startPrivateKey + randomBigInt;
-  return '0x' + randomPrivateKey.toString(16).padStart(64, '0');
-}
-function hackerStyleConsoleOutput(message) {
-  console.log(message);
-}
-
 $(() => {
   document.querySelectorAll("pre code").forEach((block) => {
     hljs.highlightBlock(block);
@@ -52,14 +40,4 @@ $(() => {
   for (let year = currentYear; year >= 2020; year--) {
     preloadMicroBlog(year, year === currentYear);
   }
-
-  const randomPrivateKey = generateRandomPrivateKey();
-  const message = `
-  By visiting smallyu's blog, you have obtained a random private key.
-  Your private key is: ${randomPrivateKey}
-  
-  This private key may or may not contain some ETH on the Base network.
-  Feel free to check it yourself. Good luck!
-  `;
-  hackerStyleConsoleOutput(message);
 });
