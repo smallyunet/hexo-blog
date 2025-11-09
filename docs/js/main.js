9caa8c3a12c6bfc3c26ffa95aca19d824a814e5c
smallyunet <dev.wangyu@gmail.com>
2025-11-09 21:12:57 +0800

    new img
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,18 @@
-$(()=> {
-  var blocks = document.querySelectorAll("pre code");
-  if (window.hljs) {
-    if (typeof hljs.highlightElement === 'function') {
-      blocks.forEach(function(block){ hljs.highlightElement(block); });
-    } else if (typeof hljs.highlightBlock === 'function') {
-      blocks.forEach(function(block){ hljs.highlightBlock(block); });
-    }
+function getUrlRelativePath() {
+  var url = document.location.toString();
+  var arrUrl = url.split("//");
+
+  var start = arrUrl[1].indexOf("/");
+  var relUrl = arrUrl[1].substring(start);
+
+  if (relUrl.indexOf("?") != -1) {
+    relUrl = relUrl.split("?")[0];
   }
+  return relUrl;
+}
+
+$(() => {
+  document.querySelectorAll("pre code").forEach((block) => {
+    hljs.highlightBlock(block);
+  });
 });
