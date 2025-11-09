dbe6b809c1bdb6502f040d02d21094ccfb3f76f6
smallyunet <dev.wangyu@gmail.com>
2025-11-09 21:57:59 +0800

    update
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,18 +1,10 @@
-function getUrlRelativePath() {
-  var url = document.location.toString();
-  var arrUrl = url.split("//");
-
-  var start = arrUrl[1].indexOf("/");
-  var relUrl = arrUrl[1].substring(start);
-
-  if (relUrl.indexOf("?") != -1) {
-    relUrl = relUrl.split("?")[0];
+$(()=> {
+  var blocks = document.querySelectorAll("pre code");
+  if (window.hljs) {
+    if (typeof hljs.highlightElement === 'function') {
+      blocks.forEach(function(block){ hljs.highlightElement(block); });
+    } else if (typeof hljs.highlightBlock === 'function') {
+      blocks.forEach(function(block){ hljs.highlightBlock(block); });
+    }
   }
-  return relUrl;
-}
-
-$(() => {
-  document.querySelectorAll("pre code").forEach((block) => {
-    hljs.highlightBlock(block);
-  });
 });
